3e98d73ec751250a176ca1bebaa1a0d5dac513bc
Aki <kieran.mortimer@gmail.com>
2020-06-12 14:42:03 +0200

    ascending sort functionality

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
-$(document).ready(function () {
-    // Parsing json file to obj arr
-    let movies = JSON.parse(movieList);
-    console.table(movies);
+// Parsing json file to obj arr
+let movies = JSON.parse(movieList);
+console.table(movies);
 
-    // Constructing HTML elements from array
+// Constructing HTML elements from array
+function addMovieCards(){
     for (let i = 0; i < movies.length; i++){
         $("#movieWrapper").append(`
             <div id="movie${i}" class="movieCard">
@@ -32,47 +32,91 @@ $(document).ready(function () {
     for (let i = 0; i < movies.length; i++){
         $(`#movie${i}`).data("likes", 0);
     }
-
+    
     console.log("Data attached");
 
-    // Displaying like counters on each movie
+    // Attaching click listener to each film
+    for (let i = 0; i < movies.length; i++){
+        $(`#movie${i}`).on("click", function(){
+            console.log(`Movie${i} has been clicked`);
+    
+            // Updating the like value for that movie
+            $(this).data("likes", $(this).data("likes") + 1);
+            console.log(`Movie${i} Likes: ${$(this).data("likes")}`);
+    
+            // Displaying the updated value
+            $(this).find(".likeCounter").text(`${$(this).data("likes")}`);
+        });
+    }
+    
+    console.log("Click listeners attached");
+}
+
+// Displaying like counters on each movie
+function setLikeCount(){
     for (let i = 0; i < movies.length; i++){
         let likeVal = $(`#movie${i}`).data("likes");
         $(`#movie${i}`).find(".likeCounter").text(likeVal);
     }
 
     console.log("Like value displayed");
+}
 
-    // Attaching click listener to each film
-    for (let i = 0; i < movies.length; i++){
-        $(`#movie${i}`).on("click", function(){
-            console.log(`Movie${i} has been clicked`);
+$(document).ready(function () {
+    // Initial loading of movies
+    addMovieCards();
+    setLikeCount();
 
-            // Updating the like value for that movie
-            $(this).data("likes", $(this).data("likes") + 1);
-            console.log(`Movie${i} Likes: ${$(this).data("likes")}`);
-
-            // Displaying the updated value
-            $(this).find(".likeCounter").text(`${$(this).data("likes")}`);
-        });
-    }
-
-    console.log("Click listeners attached");
-
-        // switch to choose behaviour
-        // jquery to get specific option at val= x
-        // do thing
-
-    // Change watcher on drop down
+    
+    
+    // Watching for a change to the sort drop down menu
     $("#sortDropDown").on("change", function() {
         console.log(`${$("#sortDropDown option:selected").val()} has been selected`);
 
-   
+        // On change determine which behaviour to enact
+        switch($("#sortDropDown option:selected").val()){
+            case "none":                
+                
+                break;
+
+            case "ascending":
+
+                // Creating an array to store the 2D likes:JqueryObj
+                let movieArr = new Array();
+               
+                // Iterating through the array and detaching the objects into the array
+                for (let i = 0; i < movies.length; i++){
+                    movieArr.push([$(`#movie${i}`).data("likes"), $(`#movie${i}`).detach()]);
+                }
+
+                console.log("Unsorted Array");
+                console.table(movieArr);
+
+                // Sort array via 0-index of each array element
+                movieArr.sort(function(a, b) {
+                    
+                    return a[0] - b[0];
+                })
+         
+                console.log("Sorted Array");
+                console.table(movieArr);
+
+                // Inserting the detached object back into HTML
+                for (movie of movieArr){
+                    $("#movieWrapper").append(movie[1]);
+                }
+
+
+               
+
+
+
+                break;
+
+            case "descending":
+
+                break;
+        }
 
     });
-
-
-
-
-
 });
