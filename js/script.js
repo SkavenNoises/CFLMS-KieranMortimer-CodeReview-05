27bf797496a488382aa44dc60ce50bf4ab5bfe97
Aki <kieran.mortimer@gmail.com>
2020-06-12 12:40:30 +0200

    like counter functionality

--- js/script.js
+++ js/script.js
@@ -18,7 +18,7 @@ $(document).ready(function () {
                         <p class="likeBtn">Like</p>
 
                         <div class="grnCircle">
-                            <p>0</p>
+                            <p class="likeCounter"></p>
                         </div>
                     </div>
                 </div>
@@ -26,10 +26,37 @@ $(document).ready(function () {
         `);
     }
 
+    console.log("Film list constructed");
+
+    // Attaching empty like counters to each movie
+    for (let i = 0; i < movies.length; i++){
+        $(`#movie${i}`).data("likes", 0);
+    }
+
+    console.log("Data attached");
+
+    // Displaying like counters on each movie
+    for (let i = 0; i < movies.length; i++){
+        let likeVal = $(`#movie${i}`).data("likes");
+        $(`#movie${i}`).find(".likeCounter").text(likeVal);
+    }
+
+    console.log("Like value displayed");
+
+    // Attaching click listener to each film
+    for (let i = 0; i < movies.length; i++){
+        $(`#movie${i}`).on("click", function(){
+            console.log(`Movie${i} has been clicked`);
+
+            // Updating the like value for that movie
+            $(this).data("likes", $(this).data("likes") + 1);
+            console.log(`Movie${i} Likes: ${$(this).data("likes")}`);
+
+            // Displaying the updated value
+            $(this).find(".likeCounter").text(`${$(this).data("likes")}`);
 
 
 
-
-
-
+        });
+    }
 });
